f6cf8dc29082c4dcc74a311fd34d3164418d251f
lamve2 <venice.lam@oocl.com>
2018-12-03 14:50:00 +0800

    finish pactise 2

--- src/App.js
+++ src/App.js
@@ -3,26 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  render() {
+  state = {number: 0}
+  updateNumber = ()=> {
+    this.setState({number: 1})
+  }
+    render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div>       
+          <button onClick={this.updateNumber}>Hello World!</button>
+          <span>number: {this.state.number}</span>    
       </div>
     );
   }
 }
+  
 
 export default App;

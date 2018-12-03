cb02c08409ae7d349367e5447e8d217795f4ac9c
lamve2 <venice.lam@oocl.com>
2018-12-03 17:13:31 +0800

    finish pratise 8

--- src/App.js
+++ src/App.js
@@ -2,18 +2,25 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import Number from './Component/Number.js';
+import Line from './Component/Line.js';
 
 class App extends Component {
-  state = {number: 1}
+  state = {number: 1, numberArray: new Array(1).fill(0)}
   updateNumber = ()=> {
-    this.setState({number: this.props.onClickApp(this.state.number)})
-    alert('now you know how to handle onclick event')
+    this.setState({
+      number: this.props.onClickApp(this.state.number), 
+      numberArray: new Array(this.state.number).fill(0)
+    })
+    //alert('now you know how to handle onclick event')
+    
   }
     render() {
     return (
       <div>       
           <button onClick={this.updateNumber}>Hello World!</button>
-          <Number/>   
+
+          <Number/>{this.state.number}   
+          {this.state.numberArray.map((v, i) =>(<Line key={i}/>))}
       </div>
     );
   }

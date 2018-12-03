e9fc08722d91030870296478cf560f736efa37af
lamve2 <venice.lam@oocl.com>
2018-12-03 14:53:41 +0800

    finish practise 3

--- src/App.js
+++ src/App.js
@@ -6,11 +6,13 @@ class App extends Component {
   state = {number: 0}
   updateNumber = ()=> {
     this.setState({number: 1})
+    alert('now you know how to handle onclick event')
   }
     render() {
     return (
       <div>       
           <button onClick={this.updateNumber}>Hello World!</button>
+
           <span>number: {this.state.number}</span>    
       </div>
     );
